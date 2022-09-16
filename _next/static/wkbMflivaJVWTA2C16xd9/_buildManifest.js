bb692d52f2e36b63d87b06d76c9f516e3ec9d466
nanqianglin <107928296+nanqianglin@users.noreply.github.com>
2022-09-16 08:39:04 +0000

    Deploying to gh-pages from @ HJFront/GotaBit-faucet@0646ddba26d531d3842cebf8e789a5b80115492b 🚀

--- _next/static/wkbMflivaJVWTA2C16xd9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/644-6e594ae6f42ca71c.js","static/chunks/pages/index-209e8afc01a0b99c.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
