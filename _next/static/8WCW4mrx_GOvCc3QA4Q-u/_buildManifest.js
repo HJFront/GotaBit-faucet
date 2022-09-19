4e8cdb66eee67125ffff214b65e91f2864c90e27
nanqianglin <107928296+nanqianglin@users.noreply.github.com>
2022-09-19 10:48:03 +0000

    Deploying to gh-pages from @ HJFront/GotaBit-faucet@a1f5d0230b904454dddf5aff1259aa4418b4665d 🚀

--- _next/static/8WCW4mrx_GOvCc3QA4Q-u/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/785-ec2b734745ffc074.js","static/chunks/pages/index-a3ba1a5c97ffb823.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/fut8fHJy1BT-28kV8XCzd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/785-ec2b734745ffc074.js","static/chunks/pages/index-c485585c484619e1.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
