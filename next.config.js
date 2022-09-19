b21e50032ee7b17c68159c5ae73484d6f7562d60
nanqiang <nanqianglin@gmail.com>
2022-09-19 18:37:58 +0800

    feat: using env secrets

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  perDayTestToken: process.env.PER_DAY_TEST_TOKEN,
   experimental: {
     images: {
       unoptimized: true,
     },
   },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH,
+    PER_DAY_TEST_TOKEN: process.env.PER_DAY_TEST_TOKEN,
+  }
 }
 
 module.exports = nextConfig
